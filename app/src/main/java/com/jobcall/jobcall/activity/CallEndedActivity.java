6fdc0edfdaec7294624026adede0b09fa15daf58
Paula (Latra) Gallucci <personadelmonton@gmail.com>
2021-05-20 00:26:48 +0200

    some stuff that I forgot to commit

---
+++ app/src/main/java/com/jobcall/jobcall/activity/CallEndedActivity.java
@@ -0,0 +1,16 @@
+package com.jobcall.jobcall.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.jobcall.jobcall.R;
+
+public class CallEndedActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_call_ended);
+    }
+}
